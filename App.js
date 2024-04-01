dc426d8b902f540b4985600ddd98f54a5c33d905
Koodohyeon <koodh6523@naver.com>
2024-04-01 12:48:22 +0900

    0401

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, ScrollView } from 'react-native';
 import HelloWorld from './components/HelloWorld';
+import Cat from './components/Cat';
+import State from './components/State';
+import PizzaTranslator from './components/PizzaTranslator';
 
+const logo = { uri: 'https://reactnative.dev/img/tiny_logo.png', width: 32, height: 32,};
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ScrollView>
+      <Image source={logo} />
+      <PizzaTranslator />
+      <State name='Run Tum Yugger' />
+      <Image source={logo} />
+      <Cat name='Maru' num='1' />
+      <Cat name='Run Tum Yugger' num='2' />
+      <Image source={logo} />
       <HelloWorld />
-    </View>
+    </ScrollView>
   );
 }
 
